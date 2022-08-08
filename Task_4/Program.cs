a79712055ca00b6b1b76368e13544942a3eec585
vivimila <violamilashevskaya@yandex.by>
2022-08-08 16:33:12 +0300

    The task 4 is selved

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+// while (a == max;)
+    if (a > b & a > c) 
+    {
+        Console.Write($"Максимальное введенное число: {a}");
+    }
+    else
+        if (b < c) 
+        {
+            Console.Write($"Максимальное введенное число: {c}");
+        }
+        else
+        {
+            Console.Write($"Максимальное введенное число: {b}");
+        }
