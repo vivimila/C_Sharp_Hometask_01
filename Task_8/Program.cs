10f8070d3bcf8d9223b9e4cb458f726d38783c40
vivimila <violamilashevskaya@yandex.by>
2022-08-08 23:02:02 +0300

    The task 8 is selved

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// https://docs.microsoft.com/ru-ru/dotnet/csharp/language-reference/statements/iteration-statements
+// Инструкция for
+// Оператор for выполняет оператор или блок операторов, пока определенное логическое выражение равно значению true. 
+// В следующем примере показана инструкция for, выполняющая тело пока целочисленный счетчик меньше трех:
+
+Console.WriteLine("Начало диапазона:1");
+Console.WriteLine("Введите конец диапазона: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=num; i++)
+    if (i % 2 == 0)
+{
+    Console.Write($"{i}  ");
+    // count++;
+}
+
+
