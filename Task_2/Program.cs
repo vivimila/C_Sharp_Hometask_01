ef18bccd2d84eff9e9ea3d3cd5a15ba22ac97e0a
vivimila <violamilashevskaya@yandex.by>
2022-08-08 15:49:32 +0300

    The task 2 is solved

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b > a) 
+{
+    Console.Write($"число {b} большее,а число {a} меньшее");
+}
+else
+{
+    Console.Write($"число {b} меньшее,а число {a} большее");
+}
